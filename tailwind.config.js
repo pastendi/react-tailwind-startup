c796ee6bdc7acd84283dea77d474d2e52e5aaa9c
Tendi <pastendisherpa26@gmail.com>
2023-04-18 17:44:14 +0545

    basic setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '1020px',
+    xl: '1440px',
+  },
+  plugins: [],
+}
